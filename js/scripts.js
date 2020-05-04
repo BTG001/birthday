f49301fa1ca47dadd9105a8166093c5192475388
BTG001 <brunokipkorir2@gmail.com>
2020-05-04 12:51:40 +0300

    modified the form to validate birthday dates, month, year, gender and to ensure no blank spaces are left

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,38 @@ function validate()
 {
   var username = document.getElementById("uname");
   var email = document.getElementById("mail");
+  var inputMonth = document.getElementById("month");
+  var inputDate = document.getElementById("date");
+  var inputYear = document.getElementById("year");
+  var radio1 = document.getElementById("man").checked;
+  var radio2 = document.getElementById("woman").checked;
 
 
-if(username.value == "" || email.value == "")
+if(username.value == "" || email.value == "" || inputDate.value == "" || inputMonth.value == "" || inputYear.value == "")
 {
   alert("No blank values allowed");
   return false;
 }
+else if(inputMonth.value <= 0 || inputMonth.value > 12)
+{
+  alert("Enter a valid month");
+  return false;
+}
+else if(inputDate.value <=0 || inputDate.value > 31)
+  {
+    alert("Enter a valid date");
+    return false;
+  }
+  else if(inputYear.value == "")
+  {
+    alert("Enter the Year");
+    return false;
+  }
+  else if((radio1 == "") && (radio2 == ""))
+  {
+    alert("Select either male or female");
+    return false
+  }
 else
 {
   true;
