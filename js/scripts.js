d7c418ec29f0bf0e5e0b67bba5e926b9c3b2f568
BTG001 <brunokipkorir2@gmail.com>
2020-05-05 19:09:26 +0300

    Added the .trim() method to cancel out the whitespaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,24 @@
-  var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
   function validate()
 {
   var username = document.getElementById("uname");
   var email = document.getElementById("mail");
-  var inputMonth = parseInt(document.getElementById("month"));
-  var inputDate = parseInt(document.getElementById("date"));
-  var inputYear = parseInt(document.getElementById("year"));
+  var inputMonth = (document.getElementById("month"));
+  var inputDate = (document.getElementById("date"));
+  var inputYear = (document.getElementById("year"));
   var radio1 = document.getElementById("man").checked;
   var radio2 = document.getElementById("woman").checked;
 
-
-  if(username.value == "" || email.value == "" || inputDate.value == "" || inputMonth.value == "" || inputYear.value == "")
+if(username.value.trim() == "" || email.value.trim() == "" || inputMonth.value.trim() == "" || inputDate.value.trim() == "" || inputYear.value.trim() == "")
 {
   alert("No blank values allowed");
   return false;
 }
-  else if((inputMonth.value <= 0 || inputMonth.value > 12) || (inputMonth.value == 2 && inputDate.value > 29))
+else if(inputMonth.value <= 0 || inputMonth.value > 12)
 {
   alert("Enter a valid month");
   return false;
 }
-  else if(inputDate.value <=0 || inputDate.value > 31)
+else if(inputDate.value <=0 || inputDate.value > 31)
   {
     alert("Enter a valid date");
     return false;
@@ -38,17 +33,9 @@
     alert("Select either male or female");
     return false
   }
-  else {
-    true;
-  }
-
-  var New = new Date(inputMonth + "/" + inputDate + "/" + inputYear);
-  var d = New.getDate()
-
-  if (radio1 == true) {
-    alert("You were born on " + daysOfWeek[d] + " and your Akan name is: " + maleNames[d]);
-  }
-  if (radio2 == true) {
-    alert("You were born on " + daysOfWeek[d] + " and your Akan name is: " + femaleNames[d]);
-  }
+  else
+{
+  true;
 }
+}
+  
