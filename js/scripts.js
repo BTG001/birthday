3f6a3cbaa837dfa23176221684f1a9f9b784eaae
BTG001 <brunokipkorir2@gmail.com>
2020-05-05 12:48:20 +0300

    Created a variable to hold the days of the week, the names of the male as well as those of the female

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-function validate()
+  var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+  function validate()
 {
   var username = document.getElementById("uname");
   var email = document.getElementById("mail");
@@ -9,17 +13,17 @@ function validate()
   var radio2 = document.getElementById("woman").checked;
 
 
-if(username.value == "" || email.value == "" || inputDate.value == "" || inputMonth.value == "" || inputYear.value == "")
+  if(username.value == "" || email.value == "" || inputDate.value == "" || inputMonth.value == "" || inputYear.value == "")
 {
   alert("No blank values allowed");
   return false;
 }
-else if((inputMonth.value <= 0 || inputMonth.value > 12) || (inputMonth.value == 2 && inputDate.value > 29))
+  else if((inputMonth.value <= 0 || inputMonth.value > 12) || (inputMonth.value == 2 && inputDate.value > 29))
 {
   alert("Enter a valid month");
   return false;
 }
-else if(inputDate.value <=0 || inputDate.value > 31)
+  else if(inputDate.value <=0 || inputDate.value > 31)
   {
     alert("Enter a valid date");
     return false;
@@ -34,7 +38,7 @@ else if(inputDate.value <=0 || inputDate.value > 31)
     alert("Select either male or female");
     return false
   }
-else
+  else
 {
   true;
 }
